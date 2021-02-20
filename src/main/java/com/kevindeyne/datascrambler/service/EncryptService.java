f5c2bd6366a26c08ac7fbf9a6d16111806646b73
Kevin Deyne <kevindeyne@gmail.com>
2021-02-19 19:15:44 -0800

    Fix jline dependency, encryption with fingerprinting for MacOS

--- src/main/java/com/kevindeyne/datascrambler/service/EncryptService.java
+++ src/main/java/com/kevindeyne/datascrambler/service/EncryptService.java
@@ -2,6 +2,11 @@ package com.kevindeyne.datascrambler.service;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.stereotype.Service;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.ComputerSystem;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -26,10 +31,34 @@ public class EncryptService {
         try {
             final NetworkInterface network = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
             byte[] mac = network.getHardwareAddress();
-            return Base64.getEncoder().encodeToString(mac);
+            if(mac != null) return Base64.getEncoder().encodeToString(mac);
+            return Base64.getEncoder().encodeToString(fingerprint().getBytes());
         } catch (SocketException | UnknownHostException e) {
             return System.getProperty("user.name");
         }
+    }
+
+    private String fingerprint() {
+        SystemInfo systemInfo = new SystemInfo();
+        OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
+        HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
+        CentralProcessor centralProcessor = hardwareAbstractionLayer.getProcessor();
+        ComputerSystem computerSystem = hardwareAbstractionLayer.getComputerSystem();
+
+        String vendor = operatingSystem.getManufacturer();
+        String processorSerialNumber = computerSystem.getSerialNumber();
+        String processorIdentifier = centralProcessor.getProcessorIdentifier().getIdentifier();
+        int processors = centralProcessor.getLogicalProcessorCount();
+
+        String delimiter = "#";
+
+        return vendor +
+                delimiter +
+                processorSerialNumber +
+                delimiter +
+                processorIdentifier +
+                delimiter +
+                processors;
     }
 
     public String encrypt(String clearText){
