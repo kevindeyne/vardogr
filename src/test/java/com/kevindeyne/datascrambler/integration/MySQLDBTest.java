fdd4164c948b9f4c3833718c97cdf8ed613c4a66
Kevin Deyne <kevindeyne@gmail.com>
2021-02-21 13:53:08 -0800

    Added MySQL test

---
+++ src/test/java/com/kevindeyne/datascrambler/integration/MySQLDBTest.java
@@ -0,0 +1,24 @@
+package com.kevindeyne.datascrambler.integration;
+
+import org.jooq.SQLDialect;
+import org.junit.Rule;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class MySQLDBTest extends AbstractDBIntegrationTest {
+
+    @Rule
+    public MySQLContainer mysql = new MySQLContainer(DockerImageName.parse("mysql"));
+
+    @Override
+    protected JdbcDatabaseContainer getDB() {
+        return mysql;
+    }
+
+    @Override
+    protected SQLDialect getDialect() {
+        return SQLDialect.MYSQL;
+    }
+}
